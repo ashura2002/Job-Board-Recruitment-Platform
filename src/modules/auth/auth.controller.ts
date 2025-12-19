f909a8154d99778512925da51fdab0369747dee5
Jmark Dayna <jemarcdayna352@gmail.com>
2025-12-19 21:02:40 +0800

    feat(login): implemented JWT in login for authentication and authorization

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from '../users/dto/create-user.dto';
 import { IUserWithOutPassword } from '../users/dto/user-response.dto';
+import { LoginDTO } from './dto/login.dto';
+import { IJwtResponse } from './types/jwt.types';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +23,14 @@ export class AuthController {
     @Body() dto: CreateUserDTO,
   ): Promise<IUserWithOutPassword> {
     return await this.authService.registerAsJobSeeker(dto);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(
+    @Body() dto: LoginDTO,
+  ): Promise<{ message: string; token: string }> {
+    const accessToken = await this.authService.login(dto);
+    return { message: 'Login Successfully', token: accessToken };
   }
 }
