b509e1369510860c9e66b0ab1e095eab53bfb4f9
Jmark Dayna <jemarcdayna352@gmail.com>
2025-12-20 14:24:52 +0800

    feat(Role-Guard): Added Role Guard For Authorization On Every Route

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,15 +9,19 @@ import { UsersService } from './users.service';
 import { IUserWithOutPassword } from './dto/user-response.dto';
 import { JwtGuard } from 'src/common/guards/Jwt.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { RolesGuard } from 'src/common/guards/role.guard';
+import { Role } from 'src/generated/prisma/enums';
+import { Roles } from 'src/common/decorators/role.decorator';
 
 @Controller('users')
 @ApiBearerAuth('access-token')
-@UseGuards(JwtGuard)
+@UseGuards(JwtGuard, RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
   @HttpCode(HttpStatus.OK)
+  @Roles(Role.Recruiter)
   async getAllUser(): Promise<IUserWithOutPassword[]> {
     return await this.usersService.getAllUsers();
   }
