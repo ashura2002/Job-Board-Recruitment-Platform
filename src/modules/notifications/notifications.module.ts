df5dc8bba9f7d10374090045492b4c0ac7a77796
Jmark Dayna <jemarcdayna352@gmail.com>
2025-12-29 19:44:43 +0800

    feat(websocket): Implemented working websocket gateway for realtime update on applying job to notify the job owner

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -5,11 +5,21 @@ import { NotificationGateway } from './notification.gateway';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { jwtConfigFactory } from 'src/config/jwt.config';
 
 @Module({
-  imports: [UsersModule, PrismaModule, JwtModule],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: jwtConfigFactory,
+    }),
+  ],
   controllers: [NotificationController],
   providers: [NotificationGateway, NotificationService],
-  exports: [NotificationService],
+  exports: [NotificationService, NotificationGateway],
 })
 export class NotificationsModule {}
