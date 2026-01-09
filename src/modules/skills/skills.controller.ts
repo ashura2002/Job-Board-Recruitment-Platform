934fef3e4919fc17acfdc1f3ad1d8cf766666feb
Jmark Dayna <jemarcdayna352@gmail.com>
2026-01-09 16:06:00 +0800

    feat(skills): enforce ownership check when updating skill

--- src/modules/skills/skills.controller.ts
+++ src/modules/skills/skills.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Put,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -20,6 +21,7 @@ import { Role } from 'src/generated/prisma/enums';
 import type { AuthUser } from 'src/common/types/auth-user';
 import { Skill } from 'src/generated/prisma/client';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { UpdateSkillDTO } from './dto/update-skill.dto';
 
 @Controller('skills')
 @UseGuards(JwtGuard, RolesGuard)
@@ -41,22 +43,28 @@ export class SkillsController {
   // endpoints for get all my skills for job seekers
   @Get('own')
   @HttpCode(HttpStatus.OK)
+  @Roles(Role.Jobseeker)
   async getAllMySkills(@Req() req: AuthUser): Promise<Skill[]> {
     const { userId } = req.user;
     return await this.skillsService.getAllMySkills(userId);
   }
 
+  // for jobseeker if user want to get the info of there own skill
   @Get('details/:skillId')
   @HttpCode(HttpStatus.OK)
+  @Roles(Role.Jobseeker)
   async getSkillbyId(
     @Param('skillId', ParseIntPipe) skillId: number,
+    @Req() req: AuthUser,
   ): Promise<Skill> {
-    return await this.skillsService.getSkillbyId(skillId);
+    const { userId } = req.user;
+    return await this.skillsService.getSkillbyId(skillId, userId);
   }
 
+  // admin can delete skills too
   @Delete(':skillId')
   @HttpCode(HttpStatus.OK)
-  @Roles(Role.Admin, Role.Recruiter)
+  @Roles(Role.Admin, Role.Jobseeker)
   async deleteSkill(
     @Param('skillId', ParseIntPipe) skillId: number,
     @Req() req: AuthUser,
@@ -64,5 +72,19 @@ export class SkillsController {
     const { userId } = req.user;
     await this.skillsService.deleteSkill(skillId, userId);
     return { message: `Skill deleted successfully` };
+  }
+
+  // only jobseerker owner of skill can update
+  @Put(':skillId')
+  @HttpCode(HttpStatus.OK)
+  @Roles(Role.Jobseeker)
+  async updateSkill(
+    @Param('skillId', ParseIntPipe) skillId: number,
+    @Req() req: AuthUser,
+    @Body() dto: UpdateSkillDTO,
+  ): Promise<{ message: string }> {
+    const { userId } = req.user;
+    await this.skillsService.updateSkillName(skillId, userId, dto);
+    return { message: 'Skill Updated Successfully' };
   }
 }
